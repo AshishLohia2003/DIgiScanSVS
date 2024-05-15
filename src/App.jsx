f760009630789f32c823f6b134e7df7b0bfb7689
AshishLohia2003 <97489009+AshishLohia2003@users.noreply.github.com>
2024-05-15 09:49:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import ImageViewer from './Components/ImageViewer';
-import ImageViewer2 from './Components/Imagewithannotation';
 import { Box } from '@mui/material';
 
 const App = () => {
@@ -8,8 +7,6 @@ const App = () => {
   return (
     <Box m="0" p="0">
       <ImageViewer />
-      {/* <ImageViewer2 /> */}
-      {/* <ImageWithAnnotations /> */}
     </Box>
   );
 };
